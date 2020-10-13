96d071e4ba2c7d86d71c83e989478e5e47e7389b
TuxFan77 <briancross@shaw.ca>
2020-10-12 21:09:51 -0600

    Implement add / delete education routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -225,4 +225,77 @@ router.delete("/experience/:exp_id", auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/profile/education
+// @desc    Update the profile education array
+// @access  Private
+router.put(
+  "/education",
+  [
+    auth,
+    [
+      body("school", "School is required").notEmpty(),
+      body("degree", "Degree is required").notEmpty(),
+      body("from", "From date is required").notEmpty(),
+      body("fieldofstudy", "Field of study is required").notEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newEducation = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.education.unshift(newEducation);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json("Server error");
+    }
+  }
+);
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    Deletes education from profile
+// @access  Private
+router.delete("/education/:edu_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    const removeIndex = profile.education
+      .map(item => item.id)
+      .indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Server error");
+  }
+});
+
 module.exports = router;
