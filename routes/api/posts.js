999e36413ee142d2e7b45655f10efa6bc6a3378d
Brian Cross <briancross@shaw.ca>
2020-11-26 11:16:06 -0700

    Begin implementing create post API route.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,9 +3,39 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const auth = require("../../middleware/auth");
 
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
 // @route   POST api/posts
 // @desc    Create a post
 // @access  Private
-router.get("/", (req, res) => res.send("Posts route"));
+router.post(
+  "/",
+  [auth, check("text", "Text is required").notEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = User.findById(req.user.id).select("-password");
+
+      const newPost = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      const post = await newPost.save();
+      res.send(post);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
