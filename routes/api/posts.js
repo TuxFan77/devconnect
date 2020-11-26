145aedd8b0d483006fb01276b0c9b211230a31d7
Brian Cross <briancross@shaw.ca>
2020-11-26 15:15:54 -0700

    Add GET routes and clean up code in POST route.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,6 +7,38 @@ const Post = require("../../models/Post");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+router.get("/", auth, async (req, res) => {
+  try {
+    return res.send(await Post.find().sort("-date"));
+  } catch (err) {
+    console.error(err);
+    res.send("Server Error");
+  }
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by ID
+// @access  Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) return res.status(404).send({ msg: "Post not found" });
+
+    res.send(post);
+  } catch (err) {
+    console.error(err);
+
+    if (err.kind === "ObjectId")
+      return res.status(404).send({ msg: "Post not found" });
+
+    res.send("Server Error");
+  }
+});
+
 // @route   POST api/posts
 // @desc    Create a post
 // @access  Private
@@ -22,17 +54,14 @@ router.post(
     try {
       const user = User.findById(req.user.id).select("-password");
 
-      const newPost = {
+      const post = new Post({
         text: req.body.text,
         name: user.name,
         avatar: user.avatar,
         user: req.user.id,
-      };
+      });
 
-      const post = new Post(newPost);
-      post.save();
-
-      res.send(post);
+      res.send(await post.save());
     } catch (err) {
       console.error(err);
       res.status(500).send("Server Error");
