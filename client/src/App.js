935ee388f7e48d6a0da26c0f77b36c68c7aee687
Brian Cross <briancross@shaw.ca>
2020-11-28 22:58:42 -0700

    Add routes for register and login pages.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 function App() {
   return (
@@ -12,6 +14,14 @@ function App() {
         <Route exact path="/">
           <Landing />
         </Route>
+        <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
       </Router>
     </>
   );
