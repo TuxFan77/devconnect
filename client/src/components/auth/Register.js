dffd5bb73c54488647d57b7e62530abeb4143544
Brian Cross <briancross@shaw.ca>
2020-11-28 23:01:24 -0700

    Implement register component.

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const Register = () => {
+  const initialFormData = {
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  const { name, email, password, password2 } = formData;
+
+  function onChange(e) {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    if (password !== password2) {
+      console.log("passwords don't match");
+      return;
+    }
+
+    setFormData(initialFormData);
+
+    const newUser = {
+      name,
+      email,
+      password,
+    };
+
+    try {
+      const config = {
+        "Content-Type": "application/json",
+      };
+      const res = await axios.post("/api/users", newUser, config);
+      console.log(res.data);
+    } catch (err) {
+      console.error(err.response.data);
+    }
+  }
+
+  return (
+    <section className="container">
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
+      </p>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            required
+            value={name}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            required
+            value={email}
+            onChange={onChange}
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            minLength="6"
+            value={password2}
+            onChange={onChange}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <a href="login.html">Sign In</a>
+      </p>
+    </section>
+  );
+};
+
+export default Register;
